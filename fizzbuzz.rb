9175719c444514cb3b4042e7afa07cec9d9ce03b
Tara Sabella <tarasabella@gmail.com>
2018-11-15 01:08:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,6 @@ def fizzbuzz(int)
   if int % 3 == 0 
     # if the number integar is divisible by 3 
     return "Fizz"
-end
 elsif int % 5 == 0 
   return "Buzz"
 elsif int % 3 == 0 && int % 5 == 0 
