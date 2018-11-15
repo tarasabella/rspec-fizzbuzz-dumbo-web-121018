85f5cf8002c8379c08a942f3c13af5f20e9c4ce8
Tara Sabella <tarasabella@gmail.com>
2018-11-15 01:15:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0 
+    return "FizzBuzz"
+  end
   if int % 3 == 0 
     # if the number integar is divisible by 3 
     return "Fizz"
@@ -8,7 +11,5 @@ def fizzbuzz(int)
 if int % 5 == 0 
     return "Buzz"
   end 
-if int % 3 == 0 && int % 5 == 0 
-    return "FizzBuzz"
-  end
+
 end
